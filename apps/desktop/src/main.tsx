1c063b02b8092fdb0cafa0a72cd96542e724f868
Mattac766383 <164365211+Mattac766383@users.noreply.github.com>
2026-08-22 19:46:46 +0200

    feat(beta): protect app with crash recovery UI

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BetaErrorBoundary } from "./BetaErrorBoundary";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BetaErrorBoundary>
+      <App />
+    </BetaErrorBoundary>
   </React.StrictMode>,
 );
